4c90606bfb5dc8a335bb749524a572224976a63d
Tony Oliverio <tony.oliverio1@gmail.com>
2024-08-23 11:56:19 -0700

    fix dates

--- apps/web/app/lib/ra-scraper/syncEventsDb.ts
+++ apps/web/app/lib/ra-scraper/syncEventsDb.ts
@@ -2,6 +2,8 @@ import {Artist, EventListing} from "./fetchEvents";
 import { db } from "../../server/db";
 import { events, venues,surveys } from "../../server/db/schema";
 import { count, sql } from "drizzle-orm";
+import { DateTime } from "luxon";
+
 
 export async function syncEventDatabase(eventListings: EventListing[]){
   console.log("syncing")
@@ -39,7 +41,18 @@ export async function syncEventDatabase(eventListings: EventListing[]){
 
   console.log(`Inserting ${toInsert.length} records`);
   // If we've already inserted some ID, we won't again.
-  return await db.insert(events).values(toInsert).onConflictDoNothing().returning({id: events.id});
+  return await db.insert(events)
+    .values(toInsert)
+    .onConflictDoUpdate({
+      target: events.id,
+      set: {
+        // This statement means that if there is a conflict on the 'id' field during the insert operation,
+        // the 'startTime' field of the existing record will be updated to the value of 'startTime' from the new record being inserted.
+        startTime: sql`excluded.startTime`,
+        endTime: sql`excluded.endTime`
+      }
+    })
+    .returning({ id: events.id });
 }
 
 export function extractEventFields(eventListing: EventListing) {
@@ -54,8 +67,8 @@ export function extractEventFields(eventListing: EventListing) {
     title: eventListing.event.payload.title,
     venueName: eventListing.event.payload.venue.data.name,
     artistNames,
-    startTime: new Date(eventListing.event.payload.startTime),
-    endTime: new Date(eventListing.event.payload.endTime),
+    startTime: DateTime.fromISO(eventListing.event.payload.startTime, { zone: 'America/New_York' }).toJSDate(),
+    endTime: DateTime.fromISO(eventListing.event.payload.endTime, { zone: 'America/New_York' }).toJSDate(),
   }
 
 }
